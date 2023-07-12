7ffdd5d3110d5224b304c285e9cda552fdc5ce5a
rawk akani <lionelmbankeu@gmail.com>
2023-07-12 12:43:54 +0200

    saved files

---
+++ deploy.js
@@ -0,0 +1,34 @@
+//https://deno.land/x/compress@v0.4.5
+import { tgz } from "https://deno.land/x/compress@v0.4.4/mod.ts";
+import{ readableStreamFromReader }from "https://deno.land/std@0.193.0/streams/mod.ts";
+
+const compress = async(path,name, deployPath) => {
+    const filePath = `./${name}.tar.gz`
+    await tgz.compress(`${path}`, filePath);
+    const src = await Deno.open(filePath);
+    const size=(await Deno.stat(filePath)).size;
+    const f = readableStreamFromReader(src)
+
+    fetch(deployPath,{
+        method: "POST",
+        headers: {
+            'content-length': `${size}`,
+            "content-type":"application/tar"
+        },
+        body:f
+    })
+
+
+}
+
+/**
+* Deploy, to VM
+* deplpoy [name of app] [build directory] [vm host url]
+*/
+  if (import.meta.main) {
+      const [name, path, deploy] = Deno.args
+      console.log("begin deployment")
+      const deployTo = Deno.env.get('deploy') ? Deno.env.get('deploy') : deploy
+      await compress(path,name, deployTo)
+      console.log("deployment done")
+  }
